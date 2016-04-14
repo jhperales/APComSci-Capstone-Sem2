c92b033e4fc8af39f6798d765b4fb34043ea7714
jhperales <JHPerales@student.naperville203.org>
2016-04-14 14:14:29 -0500

    Stared working on Card Class

---
+++ Capstone/Card.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Card
+{
+    private static final String[] card_lbl{"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
+    
+        
