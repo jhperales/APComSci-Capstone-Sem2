c1d65808f4b0fb99e308a05a025c8951b7f04507
jhperales <jhperales@stu.naperville203.org>
2016-04-17 22:22:15 -0500

    More additions

--- Capstone/Card.java
+++ Capstone/Card.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 public class Card
 {
-    private static final String[] card_lbl{"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
-    
+    private static final String[] card_lbl = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
+    private static final int[] card_vals = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+}
         
