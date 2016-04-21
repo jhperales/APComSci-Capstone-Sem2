d209968c40aae4c16ee74b9f8da3397b004b1b9f
jhperales <JHPerales@student.naperville203.org>
2016-04-21 14:04:46 -0500

    Added constructor to Card

--- Capstone/Card.java
+++ Capstone/Card.java
@@ -2,13 +2,40 @@ import java.util.*;
 import javax.swing.*;
 public class Card
 {
-    private static String[] card_lbl = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    private static int[] card_vals = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+    private static ArrayList<String> card_lbl = new ArrayList<String>();
+    private static ArrayList<Integer> card_vals = new ArrayList<Integer>();
+    
+    public Card()
+    {
+        card_lbl.add("A");
+        card_lbl.add("2");
+        card_lbl.add("3");
+        card_lbl.add("4");
+        card_lbl.add("5");
+        card_lbl.add("6");
+        card_lbl.add("7");
+        card_lbl.add("8");
+        card_lbl.add("9");
+        card_lbl.add("10");
+        card_lbl.add("K");
+        card_lbl.add("Q");
+        card_lbl.add("J");
+        card_vals.add(1);
+        card_vals.add(2);
+        card_vals.add(3);
+        card_vals.add(4);
+        card_vals.add(5);
+        card_vals.add(6);
+        card_vals.add(7);
+        card_vals.add(8);
+        card_vals.add(9);
+        card_vals.add(10);
+        card_vals.add(10);
+        card_vals.add(10);
+    }
     
     public int oneOrEleven()
     {
-        return JOptionPane.showConfirmDialog(null, "Would you like the Ace to count for a 1 or 11?", "Would you like the Ace to count for a 1 or 11?", JOptionPane.YES_NO_OPTION);
+        return JOptionPane.showConfirmDialog(null, "Would you like the Ace to count for a 1 or 11?", null, JOptionPane.YES_NO_OPTION);
     }
-    
 }
-        
--- Capstone/Player.java
+++ Capstone/Player.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 public class Player
 {
     private static int total_points;
-    private static String[] hand;
+    private static ArrayList<String> hand = new ArrayList<String>();
     
     public int getTotal()
     {
         return this.total_points;
     }
-    public String[] getHand()
+    public String getHand()
     {
-        return this.hand;
+        return this.hand.toString();
+    }
+    public void addCard(String lbl)
+    {
+        hand.add(lbl);
     }
 }
     
